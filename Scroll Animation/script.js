c66d9cf278f96add3cdb24f438f2b1611eefd2d2
Mitchell Robles <mitchellr92@live.com>
2023-01-25 04:43:56 -0800

    add if else statement to function

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -1,24 +1,19 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
+const boxes = document.querySelectorAll('.box')
 
-let load = 0
+window.addEventListener('scroll', checkBoxes)
 
-let int = setInterval(blurring, 30)
+checkBoxes()
 
-function  blurring() {
-    load++
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5 * 4)
 
-    if(load > 99) {
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top
 
-        clearInterval(int)
+    if(boxTop < triggerBottom) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
     }
-
-    loadText.innerText = `${load}%`
-    loadText.style.opractiy = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
- }
-
- // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  })
 }
