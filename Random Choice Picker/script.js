d497635d9459f3374607448ec5f23c8e958b55aa
Mitchell Robles <mitchellr92@live.com>
2023-02-03 04:39:35 -0800

    add function for picking random tag

--- Random Choice Picker/script.js
+++ Random Choice Picker/script.js
@@ -3,8 +3,16 @@ const textarea = document.getElementById("textarea");
 
 textarea.focus();
 
-textarea.addEventListener("keyiup", (e) => {
+textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -21,4 +29,41 @@ function createTags(input) {
     tagEl.innerHTML = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    hightlightTag(randomTag);
+
+    setTimeout(() => {
+      unHightlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      hightlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Match.random() * tags.length)];
+}
+
+function hightlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHightlightTag(tag) {
+  tag.classList.remove("highlight");
 }
