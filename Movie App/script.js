afa7cc90459a5319ae5ad79acb11cfffabd701b7
Mitchell Robles <mitchellr92@live.com>
2023-02-14 04:52:47 -0800

    start on function to create movie posters

--- Movie App/script.js
+++ Movie App/script.js
@@ -5,6 +5,7 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
 const SEARCH_URL =
   "https://api.themoviedb.org/3/search/moive?api_key=9e980059befe35cecefa70df4fab4c51&query='";
+const main = document.getElementById("main");
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
@@ -15,6 +16,19 @@ getMovies(API_URL);
 async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movies) => {
+    const { title, poster_path, cote_average, overview } = movies;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+  });
 }
 
 form.addEventListener("submit", (e) => {
@@ -23,7 +37,7 @@ form.addEventListener("submit", (e) => {
   const searchTerm = search.value;
 
   if (searchTerm && searchTerm !== "") {
-    getMovies(SEARCH_API + serchTerm);
+    getMovies(SEARCH_API + searchTerm);
 
     search.value = "";
   } else {
