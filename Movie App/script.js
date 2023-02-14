d9369b2f4fffd429aa359c52459b9f7fcefc3b04
Mitchell Robles <mitchellr92@live.com>
2023-02-13 21:48:10 -0800

    add fucntions

--- Movie App/script.js
+++ Movie App/script.js
@@ -4,4 +4,29 @@ const API_URL =
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
 const SEARCH_URL =
-  "https://api.themoviedb.org/3/search/moive?api_key=9e980059befe35cecefa70df4fab4c51";
+  "https://api.themoviedb.org/3/search/moive?api_key=9e980059befe35cecefa70df4fab4c51&query='";
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+// Get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + serchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
