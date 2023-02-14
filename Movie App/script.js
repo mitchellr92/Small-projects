bb238aa0fca9dbc673338b67fa84feab4ac6f9b1
Mitchell Robles <mitchellr92@live.com>
2023-02-13 21:42:49 -0800

    constants for javascript

---
+++ Movie App/script.js
@@ -0,0 +1,7 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9e980059befe35cecefa70df4fab4c51&page=1";
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_URL =
+  "https://api.themoviedb.org/3/search/moive?api_key=9e980059befe35cecefa70df4fab4c51";
